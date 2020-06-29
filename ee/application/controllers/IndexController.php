ecf4e7267394d58767869591b5ab1160087d3624
Dinesh Rabara <dinesh.rabara@gmail.com>
2020-06-29 16:44:12 +0530

    route base on controller and view

--- ee/application/controllers/IndexController.php
+++ ee/application/controllers/IndexController.php
@@ -1,18 +1,34 @@
 <?php
 
+/**
+ * IndexController
+ */
 class IndexController extends Zend_Controller_Action
 {
 
+    /**
+     * init
+     *
+     * @return void
+     */
     public function init()
     {
         /* Initialize action controller here */
     }
 
+    /**
+     * indexAction
+     *
+     * @return void
+     */
     public function indexAction()
     {
         // action body
     }
 
+    public function testAction()
+    {
+
+    }
 
 }
-
