f55c24318a555e209dcb23e73dd504ec3e09bae5
Dinesh Rabara <dinesh.rabara@gmail.com>
2020-07-06 14:08:35 +0530

    guestbook index done

---
+++ ee/application/modules/Auth/Bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Class Auth_Bootstrap
+ */
+class Auth_Bootstrap extends Zend_Application_Module_Bootstrap
+{
+
+}
--- ee/application/modules/Auth/controllers/LoginController.php
+++ ee/application/modules/Auth/controllers/LoginController.php
@@ -5,6 +5,9 @@ class Auth_LoginController extends Zend_Controller_Action
 
     public function init()
     {
+        echo "<pre>";
+        print_r('login init method');
+        exit(0);
         /* Initialize action controller here */
     }
 
