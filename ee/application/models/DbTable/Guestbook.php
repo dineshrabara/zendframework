e2372c0db3f8dc1df7286edc52e3067e7ec14080
Dinesh Rabara <dinesh.rabara@gmail.com>
2020-07-07 19:08:49 +0530

    remove extra lines

--- ee/application/models/DbTable/Guestbook.php
+++ ee/application/models/DbTable/Guestbook.php
@@ -1,10 +1,11 @@
 <?php
-
+/**
+ * Class Application_Model_DbTable_Guestbook
+ */
 class Application_Model_DbTable_Guestbook extends Zend_Db_Table_Abstract
 {
-
+    /**
+     * @var string
+     */
     protected $_name = 'guestbook';
-
-
 }
-
